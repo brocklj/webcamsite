15479743e03635dcf80ab8ada3f41521ceb8c97d
Unknown <brockljakub@gmail.com>
2017-07-16 21:48:18 +0200

    as

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def welcome
-    @images = Dir.glob("app/assets/images/webcam/*.jpg")
-    @image.sort
+    @images = (Dir.glob("app/assets/images/webcam/*.jpg")).sort
+    @images = @images
+
   end
 end
