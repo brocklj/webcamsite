9e3234b7ca4bbaf05a07d910b370367097d369b5
Unknown <brockljakub@gmail.com>
2017-07-18 13:03:40 +0200

    Change of layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,6 @@ class HomeController < ApplicationController
     @images = (Dir.glob("app/assets/images/webcam/*.jpg")).sort
     @images = @images
     @videos = (Dir.glob("app/assets/images/webcam/*.ogg")).sort
-
-
+    @video = @videos.reverse!
   end
 end
