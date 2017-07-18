10dd8344b323cdb86acde7035a307885069f15a7
Unknown <brockljakub@gmail.com>
2017-07-18 19:55:32 +0200

    final sort

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def welcome
-    @images = (Dir.glob("app/assets/images/webcam/*.jpg")).sort
-    @images = @images
+    @images = (Dir.glob("app/assets/images/webcam/*.jpg")).sort!
+    @images = @images.reverse!
     @videos = (Dir.glob("app/assets/images/webcam/*.ogg")).sort!
     @video = @videos.reverse!
   end
