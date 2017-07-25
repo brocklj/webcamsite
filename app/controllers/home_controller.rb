ea3104b356cf31070a8f72d64473b3e72f9b7243
Unknown <brockljakub@gmail.com>
2017-07-25 20:50:54 +0200

    Added pangination into shooted images

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,7 @@ class HomeController < ApplicationController
     @images = @images.reverse!
     @videos = (Dir.glob("app/assets/images/webcam/*.ogg")).sort!
     @video = @videos.reverse!
+    @current_page = params[:imgpage].to_i == nil ? 0 : params[:imgpage].to_i
+    @per_page = 6
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'home/welcome'
 
   root 'home#welcome'
+  match '/welcome' => "home#welcome", via: :pages
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
